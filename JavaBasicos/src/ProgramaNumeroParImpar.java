7c12c130d219f788ba2771b86fd9900ed33a9065
KARLA-LEON <karla.leon.resendiz.1@gmail.com>
2022-02-04 11:28:57 -0600

    ejercico con numeros pares e impares utilizando if else

---
+++ JavaBasicos/src/ProgramaNumeroParImpar.java
@@ -0,0 +1,14 @@
+public class ProgramaNumeroParImpar { 
+	
+	public static void main (String[] args) { 
+		
+		int numero1 = 17;
+		
+		if((numero1 % 2) != 0) { 
+			System.out.println("No es par"); 
+			}else { System.out.println("Es par"); 
+			
+			} 
+		} 
+	} 
+
